b38420d41cf7b0d6c1eeb4443d610b43820f6272
EZEN <EZEN@DESKTOP-7E2BD7D>
2020-01-03 17:44:12 +0900

    home.jsp HomeController 변경 xyz.jsp 추가
    by 팀장 2020-01-03 17:43

--- MyProject/src/main/java/com/MyProject/Book/HomeController.java
+++ MyProject/src/main/java/com/MyProject/Book/HomeController.java
@@ -24,13 +24,13 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+		logger.info("[Welcome home!] The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
 		String formattedDate = dateFormat.format(date);
-		
+		//model
 		model.addAttribute("serverTime", formattedDate );
 		
 		return "home";
